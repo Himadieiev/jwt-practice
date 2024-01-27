4351acbf86b8e5d6a27ede93daec26b7104caa1b
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2024-01-27 21:02:06 +0200

    add

--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,8 @@
 const Router = require('express').Router;
-const userController = require('../controllers/user-controller.js');
 const { body } = require('express-validator');
+
+const userController = require('../controllers/user-controller.js');
+const authMiddleware = require('../middlewares/auth-middleware.js');
 
 const router = new Router();
 
@@ -14,6 +16,6 @@ router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
 router.get('/refresh', userController.refresh);
-router.get('/users', userController.getUsers);
+router.get('/users', authMiddleware, userController.getUsers);
 
 module.exports = router;
