ee2fa40bf4a64c24ad1c2c00cf67eb14b2c135da
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2024-01-28 14:35:00 +0200

    add

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -19,6 +19,7 @@ $api.interceptors.response.use(
   },
   async error => {
     const originalRequest = error.config;
+    // eslint-disable-next-line eqeqeq
     if (error.response.status == 401 && error.config && !error.config._isRetry) {
       originalRequest._isRetry = true;
       try {
